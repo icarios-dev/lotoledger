6218d9ab639e3f3642614aad8b9bbd4dd64b4e87
Arnaud Pithon <arnaud@pithon.dev>
2026-01-23 05:21:46 +0100

    refactor: évolution de l'architecture
    adaptation à une archi monorepo backend / frontend / db

--- db/ingest/populate.sql
+++ db/ingest/populate.sql
@@ -15,31 +15,31 @@ truncate table work.raw5;
 -- -----------------------------------------------------------------------------
 -- legacy: 1976-2008 (6 + complementaire, date yyyymmdd)
 -- -----------------------------------------------------------------------------
-\copy work.raw1 from '../../assets/loto-1976-2008.csv' with (format csv, delimiter ';', NULL '', header true)
+\copy work.raw1 from './assets/datasets/loto-1976-2008.csv' with (format csv, delimiter ';', NULL '', header true)
 select work.ingest_raw1();
 truncate table work.raw1;
 -- -----------------------------------------------------------------------------
 -- modern: 2008-2017 (5 + chance, date dd/mm/yyyy)
 -- -----------------------------------------------------------------------------
-\copy work.raw2 from '../../assets/loto-2008-2017.csv' with (format csv, delimiter ';', NULL '', header true)
+\copy work.raw2 from './assets/datasets/loto-2008-2017.csv' with (format csv, delimiter ';', NULL '', header true)
 select work.ingest_raw2();
 truncate table work.raw2;
 -- -----------------------------------------------------------------------------
 -- modern: 2017-2019
 -- -----------------------------------------------------------------------------
-\copy work.raw34 from '../../assets/loto-2017-2019.csv' with (format csv, delimiter ';', NULL '', header true)
+\copy work.raw34 from './assets/datasets/loto-2017-2019.csv' with (format csv, delimiter ';', NULL '', header true)
 select work.ingest_raw34();
 truncate table work.raw34;
 -- -----------------------------------------------------------------------------
 -- modern: 2019-2019
 -- -----------------------------------------------------------------------------
-\copy work.raw34 from '../../assets/loto-2019-2019.csv' with (format csv, delimiter ';', NULL '', header true)
+\copy work.raw34 from './assets/datasets/loto-2019-2019.csv' with (format csv, delimiter ';', NULL '', header true)
 select work.ingest_raw34();
 truncate table work.raw34;
 -- -----------------------------------------------------------------------------
 -- modern + second: 2019-2025 (raw5)
 -- -----------------------------------------------------------------------------
-\copy work.raw5 from '../../assets/loto-2019-2025.csv' with (format csv, delimiter ';', NULL '', header true)
+\copy work.raw5 from './assets/datasets/loto-2019-2025.csv' with (format csv, delimiter ';', NULL '', header true)
 -- principal: 5 + chance
 select work.ingest_raw5();
 -- second tirage: 5, no chance
